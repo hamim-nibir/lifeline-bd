09d25490924546ac6423d49b45bdee1a89f5ce66
hamim-nibir <nhnibir.te@email.com>
2026-05-16 00:31:26 +0600

    logo changed

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -15,12 +15,22 @@ const Logo = ({ size = 40, showText = true, onPress }: LogoProps) => {
       onPress={onPress}
       className="flex-row items-center gap-2"
     >
-      
-    <Text style={{ fontSize: 18 }}>🛡️</Text>
-      <View>
-            <Text style={{ color: "#fff", fontSize: 20, fontWeight: "900", letterSpacing: 0.5 }}>অভয়</Text>
-            <Text style={{ color: "rgba(255, 255, 255, 0.61)", fontSize: 10, letterSpacing: 1 }}>LIFELINE BD</Text>
-          </View>
+      <Image
+        source={require("../../assets/logo.png")}
+        style={{
+          width: size,
+          height: size,
+          resizeMode: "contain",
+        }}
+      />
+
+      {/* <Text style={{ fontSize: 18 }}>🛡️</Text> */}
+
+      {showText && (
+        <Text className="text-white text-2xl font-bold">
+          Lifeline BD
+        </Text>
+      )}
     </Container>
   );
 };
